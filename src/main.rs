b257b6380853c75e976df5730114cfaa74923483
Alan <asdfqzq@gmail.com>
2025-05-27 12:05:32 -0700

    Add changes

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,12 @@ use std::io::Write;
 
 async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let (event, _context) = event.into_parts();
-    let first_name = event["firstName"].as_str().unwrap_or("world");
 
-    println!("{event:?}");
-    println!("-----------------");
-    println!("{event:#?}");
+    println!("event: {event:?}");
     println!("-----------------");
 
-    // Actual procedure
-    let args: Vec<&str> = event["body"].as_str().unwrap().split("_").collect();
+    let body = event["body"].as_str().expect("event needs body field");
+    let args: Vec<&str> = body.split("_").collect();
     let api_key = args[0];
     let start_pos = args[1];
     let moves = args[2];
@@ -39,7 +36,6 @@ async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
     if api_key != API_KEY {
         eprintln!("Invalid api_key: {api_key}");
     }
-
 
     // Try running stockfish
     let mut stockfish = Command::new("/var/task/stockfish")
@@ -52,9 +48,12 @@ async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
     stdin.write_all("uci".as_bytes()).expect("Failed to write to stdin");
     stdin.write_all("ucinewgame".as_bytes()).expect("Failed to write to stdin");
 
+    println!("write_all calls complete");
+    stdin.flush();
+
     let output = stockfish.wait_with_output().expect("Failed to read stdout");
     let output = String::from_utf8_lossy(&output.stdout);
     println!("output = {output}");
 
-    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
+    Ok(json!({ "message": "success" }))
 }
