c156c1c94fbd96bb98080e9f148f43022bd3070f
Alan <asdfqzq@gmail.com>
2025-05-28 07:51:59 -0700

    Use BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 // Copy & pasted from https://github.com/awslabs/aws-lambda-rust-runtime
-#[allow(unused_variables)]
 
 use std::env;
 
-use lambda_runtime::{service_fn, LambdaEvent, Error};
+use lambda_runtime::{service_fn, LambdaEvent, Error as LambdaError};
 use serde_json::{json, Value};
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), LambdaError> {
     unsafe {
         env::set_var("RUST_BACKTRACE", "1");
     }
@@ -20,10 +19,33 @@ async fn main() -> Result<(), Error> {
 const _API_KEY: &'static str = "nfY4b672";
 
 use std::process::{Command, Stdio};
-use std::io::{Read, Write};
-use std::{thread, time::Duration};
+use std::io::{BufRead, BufReader};
 
-async fn my_lambda_func(_event: LambdaEvent<Value>) -> Result<Value, Error> {
+fn get_stockfish_process() -> Result<std::process::Child, std::io::Error> {
+    let on_windows = cfg!(target_os = "windows");
+    let stockfish_path = if on_windows {
+        "./stockfish-windows.exe"
+    } else {
+        "/var/task/stockfish-linux"
+    };
+
+    Ok(if on_windows {
+        Command::new(stockfish_path)
+            // .args(&["/k", stockfish_path])
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("failed to spawn stockfish child process")
+    } else {
+        Command::new(stockfish_path)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("failed to spawn stockfish child process")
+    })
+}
+
+async fn my_lambda_func(_event: LambdaEvent<Value>) -> Result<Value, LambdaError> {
     // let (event, _context) = event.into_parts();
     // let body = event["body"].as_str().expect("event needs body field");
     // let args: Vec<&str> = body.split("_").collect();
@@ -37,70 +59,25 @@ async fn my_lambda_func(_event: LambdaEvent<Value>) -> Result<Value, Error> {
     // let output = dir_output.stdout;
     // let output = String::from_utf8_lossy(&output);
     // println!("{output}");
+    let mut stockfish = get_stockfish_process().expect("failed to run stockfish");
 
-    let on_windows = cfg!(target_os = "windows");
+    let _stdin = stockfish.stdin.take().expect("failed to open stdin");
+    let stdout = stockfish.stdout.take().expect("failed to open stdout");
 
-    let stockfish_path = if on_windows {
-        "./stockfish-windows.exe"
-    } else {
-        "/var/task/stockfish-linux"
-    };
+    let mut buf = String::new();
+    let mut reader = BufReader::new(stdout);
+    reader.read_line(&mut buf).expect("error reading line from stdout");
 
-    let mut stockfish;
-    if on_windows {
-        stockfish = Command::new(stockfish_path)
-            // .args(&["/k", stockfish_path])
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("failed to spawn stockfish child process"); 
-    } else {
-        stockfish = Command::new(stockfish_path)
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("failed to spawn stockfish child process");
-    };
+    println!("buf: {buf}");
 
-    let mut stdin = stockfish.stdin.take().expect("failed to open stdin");
-    let mut stdout = stockfish.stdout.take().expect("failed to open stdout");
-
-    //////////////////
-    let mut buf = Vec::new();
-    let read_count = stdout.read(&mut buf).expect("couldn't read from stdout");
-    let output = String::from_utf8_lossy(&buf);
-    println!("output = {output}");
-    println!("read_count: {read_count}");
-    println!("buf: {buf:?}");
-    // End of initial
-
-    stdin.write_all("uci".as_bytes()).expect("failed to write to stdin");
-    stdin.write_all("ucinewgame".as_bytes()).expect("failed to write to stdin");
-
-    println!("write_all calls complete");
-    stdin.flush().expect("error when flushing output to stdin");
-
-    let mut buf = Vec::new();
-    stdout.read(&mut buf).expect("couldn't read from stdout");
-    let output = String::from_utf8_lossy(&buf);
-    println!("output = {output}");
-
-    // Input moves
-    stdin.write_all("go depth 15".as_bytes()).expect("failed to write to stdin");
-    stdin.flush().expect("error when flushing output to stdin");
-
-    println!("about to sleep");
-    thread::sleep(Duration::from_millis(500));
-
-    let mut buf = Vec::new();
-    stdout.read(&mut buf).expect("couldn't read from stdout");
-    let output = String::from_utf8_lossy(&buf);
-    println!("output = {output}");
-
-    println!("Finished");
+    // //////////////////
+    // let mut buf: [u8; 50] = [0; 50];
+    // let read_count = stdout.read(&mut buf).expect("couldn't read from stdout");
+    // let output = String::from_utf8_lossy(&buf);
+    // println!("output = {output}");
+    // println!("read_count: {read_count}");
+    // println!("buf: {buf:?}");
     Ok(json!({ "message": "success" }))
-
-
 }
 
 #[tokio::test]
